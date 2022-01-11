adc755258001c73daa7f1eaf48beea6c6e94173b
Saksit Sawang <hanamiza555@gmail.com>
2022-01-11 16:37:25 +0000

    update misterm project detail

--- src/pages/chap0/score.tsx
+++ src/pages/chap0/score.tsx
@@ -7,32 +7,42 @@ import { Page, PageSection, PageSubSection } from '../../components/Page'
 
 const ScorePage: NextPage = () => (
   <Page slug="score">
-    <PageSection id="midterm-project" title="Midterm project">
-      <PageSubSection title="30% - Front-end Headless CMS">
-        <Alert severity="info" sx={{ border: (theme: Theme) => `1px solid ${theme.palette.info.main}`, mt: 2, '& > *': { alignItems: 'center' } }}>
+    <PageSection id="midterm-project" title="Midterm project (30%)">
+      <PageSubSection title="Front-end CMS">
+        <Alert severity="info" sx={{ border: (theme: Theme) => `1px solid ${theme.palette.info.main}`, my: 2, '& > *': { alignItems: 'center' } }}>
           <AlertTitle sx={{ m: 0 }}>
             Project member: 1 member
           </AlertTitle>
         </Alert>
+        Implement front-end (React or Next.js) for CMS (Content Management System) using content from CMS API
+        <ul>
+          <li>Home page</li>
+          <li>Post content page</li>
+          <li>Tag/Category page</li>
+          <li>Author page</li>
+        </ul>
       </PageSubSection>
-      <PageSubSection title="Detail">
-        Midterm project detail
+      <PageSubSection title="Deadline">
+        27 Feb 2022 23:59:59
       </PageSubSection>
     </PageSection>
-    <PageSection id="final-project" title="Final project">
-      <PageSubSection title="40% - Full-stack">
-        <Alert severity="info" sx={{ border: (theme: Theme) => `1px solid ${theme.palette.info.main}`, mt: 2, '& > *': { alignItems: 'center' } }}>
+    <PageSection id="final-project" title="Final project (40%)">
+      <PageSubSection title="Full-stack">
+        <Alert severity="info" sx={{ border: (theme: Theme) => `1px solid ${theme.palette.info.main}`, my: 2, '& > *': { alignItems: 'center' } }}>
           <AlertTitle sx={{ m: 0 }}>
             Project members: 4 members
           </AlertTitle>
         </Alert>
+        Project detail: TBD
       </PageSubSection>
-      <PageSubSection title="Detail">
-        TBD
+      <PageSubSection title="Deadline">
+        1 May 2022 23:59:59
       </PageSubSection>
     </PageSection>
-    <PageSection id="final-exam" title="Final exam">
-      <PageSubSection title="30% - Exam" />
+    <PageSection id="final-exam" title="Final exam (30%)">
+      <PageSubSection title="Exam topics">
+        TBD
+      </PageSubSection>
     </PageSection>
   </Page>
 )
