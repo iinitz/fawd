12b5c82e59528683ed2785988a31e4b503512f55
Saksit Sawang <hanamiza555@gmail.com>
2022-02-02 05:06:05 +0000

    update useState docs

--- src/pages/chap3/state.tsx
+++ src/pages/chap3/state.tsx
@@ -2,8 +2,10 @@ import Typography from '@mui/material/Typography'
 import { GetStaticProps, NextPage } from 'next'
 
 import { Link } from '../../components/@mui/Link'
-import { CodeBlock } from '../../components/Code'
-import { Page, PageReferences, PageReferencesLink } from '../../components/Page'
+import { Code, CodeBlock } from '../../components/Code'
+import {
+  Page, PageReferences, PageReferencesLink, PageSubSection,
+} from '../../components/Page'
 import { PageSection } from '../../components/Page/PageSection'
 
 const useStateCode = `
@@ -19,13 +21,48 @@ const Counter = () => {
   )
 }
 `
+const setStateCode = `
+const Counter = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount)
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  )
+}
+`
+const initialStateCode = `
+const [state, setState] = useState(
+  () => {
+    const initialState = someExpensiveComputation(props)
+    return initialState
+  }
+)
+`
 const StatePage: NextPage = () => (
   <Page slug="state">
-    <PageSection id="component-state" title="Component state?">
-      <Typography gutterBottom>Declare a state variable inside your component</Typography>
-    </PageSection>
     <PageSection id="use-state" title="useState">
+      <Typography gutterBottom>
+        Returns a stateful value, and a function to update it.
+      </Typography>
+      <Typography gutterBottom>
+        During the initial render, the returned state (<Code>state</Code>) is the same as the value passed as the first argument (<Code>initialState</Code>).
+      </Typography>
+      <Typography>
+        The <Code>setState</Code> function is used to update the state. It accepts a new state value and enqueues a re-render of the component
+      </Typography>
       <CodeBlock language="jsx" code={useStateCode} />
+      <PageSubSection title="Functional updates">
+        If the new state is computed using the previous state, you can pass a function to <Code>setState</Code>. The function will receive the previous value, and return an updated value. Here&apos;s an example of a counter component that uses both forms of <Code>setState</Code>
+        <CodeBlock language="jsx" code={setStateCode} />
+      </PageSubSection>
+      <PageSubSection title="Lazy initial state">
+        The <Code>initialState</Code> argument is the state used during the initial render. In subsequent renders, it is disregarded. If the initial state is the result of an expensive computation, you may provide a function instead, which will be executed only on the initial render
+        <CodeBlock language="jsx" code={initialStateCode} />
+      </PageSubSection>
     </PageSection>
     <PageSection id="read-more" title="Read more">
       <ul>
@@ -35,6 +72,7 @@ const StatePage: NextPage = () => (
     <PageReferences>
       <PageReferencesLink href="https://beta.reactjs.org/reference/usestate" title="React APIs: useState()" />
       <PageReferencesLink href="https://beta.reactjs.org/learn/managing-state" title="React Docs: Managing State" />
+      <PageReferencesLink href="https://reactjs.org/docs/hooks-reference.html#usestate" title="React Docs: Hooks API Reference" />
     </PageReferences>
   </Page>
 )
